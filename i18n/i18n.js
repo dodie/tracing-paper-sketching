13f94863a2478546ac515463e9cc4bd6f9d84a46
Tanmay Rajore <54497541+TanmayRajore@users.noreply.github.com>
2019-10-11 02:41:19 +0530

    Update i18n.js

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -2,9 +2,11 @@ import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
 import en from './translations/en.json';
 import pl from './translations/pl.json';
+import de from './translations/de.json';
 
 i18n.fallbacks = true;
 i18n.translations = { en, pl };
+i18n.translations = { en, de };
 i18n.locale = Localization.locale;
 i18n.defaultLocale = 'en-EN';
 
