955db2ac8f74a19d691708d6ee28ff766ccb06cc
Tanmay Rajore <54497541+TanmayRajore@users.noreply.github.com>
2019-10-11 02:33:46 +0530

    Update i18n.js

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -3,10 +3,12 @@ import i18n from 'i18n-js';
 import en from './translations/en.json';
 import pl from './translations/pl.json';
 import ru from './translations/ru.json';
+import es from './translations/es.json';
 
 i18n.fallbacks = true;
 i18n.translations = { en, pl };
 i18n.translations = { en, ru };
+i18n.translations = { en, es };
 i18n.locale = Localization.locale;
 i18n.defaultLocale = 'en-EN';
 
