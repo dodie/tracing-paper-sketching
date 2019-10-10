6604181520df423b7aae08b978db4e86ab2e1bcc
karorogunso <karorogunsou@gmail.com>
2019-10-11 06:43:23 +0700

    Thai language

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -2,9 +2,10 @@ import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
 import en from './translations/en.json';
 import pl from './translations/pl.json';
+import th from './translations/th.json';
 
 i18n.fallbacks = true;
-i18n.translations = { en, pl };
+i18n.translations = { en, pl, th };
 i18n.locale = Localization.locale;
 i18n.defaultLocale = 'en-EN';
 
