e8d0b6a3eb32f8be00c4644d3c6840fb94d08ecb
xit4 <xit4@users.noreply.github.com>
2019-10-11 16:14:51 +0200

    added it.json with italian translations

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,11 +1,12 @@
-import * as Localization from 'expo-localization';
-import i18n from 'i18n-js';
-import en from './translations/en.json';
-import pl from './translations/pl.json';
+import * as Localization from "expo-localization";
+import i18n from "i18n-js";
+import en from "./translations/en.json";
+import pl from "./translations/pl.json";
+import it from "./translations/it.json";
 
 i18n.fallbacks = true;
-i18n.translations = { en, pl };
+i18n.translations = { en, pl, it };
 i18n.locale = Localization.locale;
-i18n.defaultLocale = 'en-EN';
+i18n.defaultLocale = "en-EN";
 
 export default i18n;
