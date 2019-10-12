705f51820e50c78ad730b41e10c7be11fa7a101a
Fabrizio Vitelli <fabvit86@yahoo.it>
2019-10-12 17:50:19 +0200

    add italian translation

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -6,7 +6,7 @@ import th from './translations/th.json';
 import ru from './translations/ru.json';
 
 i18n.fallbacks = true;
-i18n.translations = { en, pl, ru, th };
+i18n.translations = { en, pl, ru, th, it };
 i18n.locale = Localization.locale;
 i18n.defaultLocale = 'en-EN';
 
