a1a521ecffb55441cb676422906a1b231a20a699
Dávid Csákvári <dodiehun@gmail.com>
2019-10-12 19:38:25 +0200

    add Japanese translations

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -6,9 +6,10 @@ import pt_BR from './translations/pt_BR.json';
 import th from './translations/th.json';
 import ru from './translations/ru.json';
 import sr from "./translations/sr.json";
+import ja from "./translations/ja.json";
 
 i18n.fallbacks = true;
-i18n.translations = { en, pl, ru, th, pt_BR, sr };
+i18n.translations = { en, pl, ru, th, pt_BR, sr, ja };
 i18n.locale = Localization.locale;
 i18n.defaultLocale = "en-EN";
 
