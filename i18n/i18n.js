b8a905c9c3ebfea96757cc66774cd7b64a0f7a4b
Alexis Morin <the.alexis.morin@gmail.com>
2019-10-13 14:43:46 -0400

    add Chinese translation (simplified)

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,27 +1,49 @@
-import * as Localization from 'expo-localization';
-import i18n from 'i18n-js';
-import en from './translations/en.json';
-import pl from './translations/pl.json';
-import pt_BR from './translations/pt_BR.json';
-import th from './translations/th.json';
-import ru from './translations/ru.json';
+import * as Localization from "expo-localization";
+import i18n from "i18n-js";
+import en from "./translations/en.json";
+import pl from "./translations/pl.json";
+import pt_BR from "./translations/pt_BR.json";
+import th from "./translations/th.json";
+import ru from "./translations/ru.json";
 import sr from "./translations/sr.json";
 import ja from "./translations/ja.json";
 import de from "./translations/de.json";
 import nl from "./translations/nl.json";
 import sk from "./translations/sk.json";
 import es from "./translations/es.json";
-import it from './translations/it.json';
-import hu from './translations/hu.json';
-import fr from './translations/fr.json';
-import cz from './translations/cz.json';
-import ar from './translations/ar.json';
-import bn from './translations/bn.json';
+import it from "./translations/it.json";
+import hu from "./translations/hu.json";
+import fr from "./translations/fr.json";
+import cz from "./translations/cz.json";
+import ar from "./translations/ar.json";
+import bn from "./translations/bn.json";
 import hi from "./translations/hi.json";
 import id from "./translations/id.json";
+import zh from "./translations/zh.json";
 
 i18n.fallbacks = true;
-i18n.translations = { en, pl, ru, th, pt_BR, sr, ja, de, nl, sk, es, it, hu, fr, cz, ar, bn, hi, id };
+i18n.translations = {
+  en,
+  pl,
+  ru,
+  th,
+  pt_BR,
+  sr,
+  ja,
+  de,
+  nl,
+  sk,
+  es,
+  it,
+  hu,
+  fr,
+  cz,
+  ar,
+  bn,
+  hi,
+  id,
+  zh
+};
 i18n.locale = Localization.locale;
 i18n.defaultLocale = "en-EN";
 
