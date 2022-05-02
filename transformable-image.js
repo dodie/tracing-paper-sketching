c3a372e82101db3da01f28ebbef36fb25ce37fce
Dávid Csákvári <dodiehun@gmail.com>
2022-05-02 23:03:33 +0200

    Remove unused imports

--- transformable-image.js
+++ transformable-image.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Animated, Dimensions, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { Animated, Dimensions, StyleSheet } from 'react-native';
 import {
   PanGestureHandler,
   PinchGestureHandler,
