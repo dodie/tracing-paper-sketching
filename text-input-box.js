7ca40720a620d682e1f7a9f12d9b30d48861a171
Yeshan Kaushik <yeshankaushikyk@gmail.com>
2022-05-03 10:46:45 +0530

    Polished the style of TextInputBox

--- text-input-box.js
+++ text-input-box.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput} from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import ActionButtonWithText from './action-button-with-text';
 import Styles from './styles';
 import i18n from './i18n/i18n';
@@ -14,32 +14,32 @@ export default class TextInputBox extends Component {
   render() {
     return (
       <View>
-        <TextInput
-          multiline={true}
-          numberOfLines={10}
-          style={styles.input}
-          onChangeText={(text) => this.setState({ text })}
-        />
-        <Text style={styles.text}>
-          Debug: 
-          {this.state.text}
-        </Text>
-        <ActionButtonWithText onPress={() =>  this.props.onSubmitPress({text: this.state.text})} value={this.state.text} text={i18n.t("submit")}/>
+        <View style={styles.container}>
+          <Text style={styles.text}>Enter Text for displaying as an image: </Text>
+          <TextInput
+            multiline={true}
+            numberOfLines={10}
+            style={styles.input}
+            onChangeText={(text) => this.setState({ text })}
+          />
+        </View>
 
+        <ActionButtonWithText onPress={() => this.props.onSubmitPress({ text: this.state.text })} value={this.state.text} text={i18n.t("submit")} />
       </View>
     );
   }
 }
 
 const styles = Styles.create({
-
+  container: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#FFF',
+  },
   input: {
     height: 200,
     width: 300,
     textAlignVertical: 'top',
-    borderWidth: 1,
-    borderRadius: 2,
-    borderColor: '#FFF',
     color: 'white',
     padding: 10,
     fontSize: 20
@@ -47,7 +47,8 @@ const styles = Styles.create({
   text: {
     fontWeight: 'bold',
     paddingLeft: 7,
-    color: 'white'
+    color: 'white',
+    fontSize: 20
   }
 });
 
